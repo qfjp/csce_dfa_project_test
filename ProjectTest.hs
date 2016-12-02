dd4c1ab926d0412d5e8ca62d7998a8833490c538
DJP <djpade@gmail.com>
2016-12-02 18:36:16 -0500

    Comment unit tests

--- ProjectTest.hs
+++ ProjectTest.hs
@@ -36,6 +36,48 @@ import System.FilePath
 import Text.Parsec
 import Text.Parsec.String
 
+
+------------------------{ INSTANCE UNIT TESTS }-----------------------
+------------------------{ IGNORE THIS SECTION }-----------------------
+-- import Test.Hspec
+-- import Test.Hspec.Checkers
+-- import Test.QuickCheck hiding (Result)
+-- import Test.QuickCheck.Checkers
+-- import Test.QuickCheck.Classes
+--
+-- instance Arbitrary RunType where
+--     arbitrary
+--       = frequency $ map (\x -> (1, return x))
+--                         [ RunTypeUndefined, Simulate, Minimize
+--                         , Searcher, BoolopComp, BoolopProd, Invhom
+--                         , Properties ]
+--
+-- instance EqProp RunType where
+--     (=-=) = eq
+--
+-- rtSpec :: SpecWith ()
+-- rtSpec
+--   = describe "RunType ?Monoid?" $
+--       testBatch $ monoid (undefined :: RunType)
+--
+-- instance Arbitrary a => Arbitrary (ProgramExecution a) where
+--     arbitrary = PE Simulate <$> arbitrary <*> arbitrary
+--
+-- instance Eq a => EqProp (ProgramExecution a) where
+--     (=-=) = eq
+--
+-- peSpec :: SpecWith ()
+-- peSpec
+--   = describe "Program Execution ?Monad?" $ do
+--       testBatch $ functor (undefined :: ProgramExecution (Int, String, Char))
+--       testBatch $ applicative (undefined :: ProgramExecution (Int, String, Double))
+--       testBatch $ monad (undefined :: ProgramExecution (Int, String, Double))
+--       it "  <*> == ap" $
+--         property $ \x y -> (x <*> y)
+--           == ((x :: ProgramExecution (Int -> Int))
+--                `ap` (y :: ProgramExecution Int))
+----------------------------------------------------------------------
+
  -- dfas to feed to simulator
 simTestCases :: [String]
 simTestCases
