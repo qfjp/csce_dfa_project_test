0e1ef65dae89ad42bdf15c92924c8efc5d87fddf
DJP <djpade@gmail.com>
2016-12-09 13:21:12 -0500

    Add haddock documentation to ProgramExecution.hs

--- src/ProgramExecution.hs
+++ src/ProgramExecution.hs
@@ -1,24 +1,41 @@
-module ProgramExecution where
+module ProgramExecution (
+    -- * Program Execution Container
+      ProgramExecution(..)
+    , showProgExec
+    -- * Project task tags
+    , RunType(..)
+    , rtToFile
+    -- * Execution results
+    , Result(..)
+    , resultToText
+    ) where
 
 import Data.Monoid (Sum(..), (<>))
 
+-- | Type for different portions of the project
 data RunType
+    -- | Continued is only available as the identity, representing
+    -- an execution that already is typed.
     = Continued
     | Simulate
     | Minimize
     | Searcher
-    | BoolopComp
-    | BoolopProd
+    | BoolopComp -- ^ Part 1 of the Boolop task
+    | BoolopProd -- ^ Part 2 of the Boolop task
     | Invhom
     | Properties
   deriving (Eq, Show)
 
 instance Monoid RunType where
     mempty = Continued
+    -- Continued is technically only a left identity, but for
+    -- convenience we use it on the right as well
     Continued `mappend` x = x
     x `mappend` Continued = x
     x `mappend` _ = x
 
+-- | This converts RunType values into the names given on the project
+-- description
 rtToFile :: RunType -> String
 rtToFile Simulate   = "simulator"
 rtToFile Minimize   = "minimizer"
@@ -29,9 +46,14 @@ rtToFile Invhom     = "invhom"
 rtToFile Properties = "properties"
 rtToFile Continued  = "UNDEFINED"
 
+-- | A ProgramExecution is a test run over one task of the project.
 data ProgramExecution a b
-    = PE { _tag :: RunType
+    = PE { -- | The task which is being tested
+            _tag :: RunType
+           -- | A monoid that keeps track of errors. Most likely
+           -- Data.Monoid.Sum Int
          , _errorCount :: a
+           -- | The result of the execution. Most likely 'Result'
          , _result :: b }
     deriving (Eq, Show)
 
@@ -49,6 +71,8 @@ instance Monoid a => Monad (ProgramExecution a) where
       = let PE t' e' r' = f r
         in PE (t <> t') (e <> e') r'
 
+-- | The function to show the results of a ProgramExecution. Use at
+-- the end of the comments.txt file
 showProgExec :: (Enum a, Show a) => ProgramExecution (Sum Int) a -> String
 showProgExec PE {_tag = t, _errorCount = (Sum e), _result = r}
   = show t ++ ": " ++ resultToText t result ++
@@ -57,24 +81,24 @@ showProgExec PE {_tag = t, _errorCount = (Sum e), _result = r}
   where result = toEnum . fromEnum $ r
 
 data Result
+  -- | not implemented at all ($prog.txt file does not exist)
   = NotImplemented
+  -- | $prog.txt file exists, but there was an error parsing it
   | ParseError
+  -- | $prog.txt parsed OK, but the build failed (error return value)
   | BuildFail
+  -- | $prog built OK, but execution timed out at least once
   | TimeOut
+  -- | $prog execution always completed (but there were errors)
   | FinishWithError
+  -- | $prog execution always completed without errors
   | FinishPerfect
+  -- | Useful to avoid partial functions with the Enum instance
   | Unknown
   deriving (Enum, Eq, Show)
 
--- Holds which programs were implemented and what progress was made on
--- each:
--- Values:
---    0 - not implemented at all ($prog.txt file does not exist)
---    1 - $prog.txt file exists, but there was an error parsing it
---    2 - $prog.txt parsed OK, but the build failed (error return value)
---    3 - $prog built OK, but execution timed out at least once
---    4 - $prog execution always completed (but there were errors)
---    5 - $prog execution always completed without errors
+-- | Convert a 'RunType' and a 'Result' into a user friendly
+-- description of what went wrong.
 resultToText :: RunType -> Result -> String
 resultToText rt NotImplemented
   = "not implemented -- " ++ rtToFile rt ++ ".txt does not exist"
