598cfb41b60711aa59ee2c4a6abd342c090b5292
DJP <djpade@gmail.com>
2016-12-21 21:12:46 -0500

    Improve windows build process
    - Update appveyor with proper test command
    - Fix makefile comments and extra commands for better windows
      compatibility
    TODO: Fix subdirectory build to enable both windows/linux build

--- build.mk
+++ build.mk
@@ -3,8 +3,8 @@ all :
 	cd c_files; $(MAKE)
 
 test :
-	# The ugly echo "" hack is to avoid complaints about 0 being an
-	# invalid file descriptor on windows
+# The ugly echo "" hack is to avoid complaints about 0 being an
+# invalid file descriptor on windows
 	echo "" | stack --no-terminal test ":spec" --bench --no-run-benchmarks --haddock --no-haddock-deps --coverage
 
 clean :
