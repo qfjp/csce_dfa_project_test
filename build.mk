b0b30e2582eb6fa879ddbd2b86aea3fb0ae499b0
DJP <djpade@gmail.com>
2016-12-21 19:50:09 -0500

    Modify makefiles to improve windows compatibility

--- build.mk
+++ build.mk
@@ -1,14 +1,14 @@
-all:
+all :
 	stack build --coverage
 	cd c_files; $(MAKE)
 
-test:
+test :
 	# The ugly echo "" hack is to avoid complaints about 0 being an
 	# invalid file descriptor on windows
 	echo "" | stack --no-terminal test ":spec" --bench --no-run-benchmarks --haddock --no-haddock-deps --coverage
 
-clean:
+clean :
 	stack clean
 	cd c_files; $(MAKE) clean
 
-.PHONY: clean test
+.PHONY : clean test
