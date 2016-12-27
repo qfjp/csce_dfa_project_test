a7d17902071d21a27e4575658be8d9fd1c58d872
DJP <djpade@gmail.com>
2016-12-27 15:01:02 -0500

    Add node to configure for testing before build

--- cabal.mk
+++ cabal.mk
@@ -1,9 +1,10 @@
+CABALVER := $(shell cabal --numeric-version)
+
 cabal-all : cabal-init
 	cabal build
 	cabal copy
 
 cabal-test : cabal-init cabal-all
-	cabal configure --enable-tests --enable-benchmarks -v2
 	cabal test
 	cabal check
 
@@ -14,4 +15,7 @@ cabal-init : isDFA
 	cabal install --only-dependencies --enable-tests --enable-benchmarks --force-reinstalls --reorder-goals --max-backjumps=-1
 	cabal configure
 
-.PHONY : cabal-all cabal-clean cabal-init cabal-test
+cabal-test-init:
+	cabal configure --enable-tests --enable-benchmarks -v2
+
+.PHONY : cabal-all cabal-clean cabal-init cabal-test cabal-test-init
