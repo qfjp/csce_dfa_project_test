57988be1e6fc0c132be58414ddfd42af57fbcbc0
DJP <djpade@gmail.com>
2016-12-31 14:29:18 -0500

    Revert Options to include Data.Default instance

--- src/Arguments.hs
+++ src/Arguments.hs
@@ -7,22 +7,34 @@ first invocation. Options are available for single-user and batch modes
 -}
 module Arguments (Options(..), parseArgs, printHelp) where
 
-import Data.Foldable (foldlM)
-import System.Console.GetOpt
-import System.Environment (getArgs, getEnv, getProgName)
-import System.Exit (exitSuccess)
+import           Data.Default
+import           Data.Foldable         (foldlM)
+
+import           System.Console.GetOpt
+import           System.Environment    (getArgs, getEnv, getProgName)
+import           System.Exit           (exitSuccess)
 
 -- | The record storing command line options
 data Options
     = Options { -- | Single user test mode flag
-                _selftest :: Bool
+                _selftest   :: Bool
                 -- | The location of the test suite
-              , _testdir :: IO FilePath
+              , _testdir    :: IO FilePath
                 -- | The location of the project to test
-              , _progdir :: FilePath
+              , _progdir    :: FilePath
                 -- | Whether any options have been invoked
               , _optionsSet :: Bool
               }
+
+-- | The default state of the options record without invoking
+-- any flags
+instance Default Options where
+    def
+      = Options { _selftest = False
+                , _testdir = defBaseDir
+                , _progdir = "."
+                , _optionsSet = False
+                }
 
 -- | Command line options
 options :: [OptDescr (Options -> IO Options)]
@@ -52,18 +64,8 @@ parseArgs
   = do
       argv <- getArgs
       case getOpt RequireOrder options argv of
-        (opts, _, []) -> foldlM (flip id) defaultOptions opts
-        (_, _, errs) -> ioError (userError (concat errs))
-
--- | The default state of the options record without invoking
--- any flags
-defaultOptions :: Options
-defaultOptions
-  = Options { _selftest = False
-            , _testdir = defBaseDir
-            , _progdir = "."
-            , _optionsSet = False
-            }
+        (opts, _, []) -> foldlM (flip id) def opts
+        (_, _, errs)  -> ioError (userError (concat errs))
 
 -- | Set the location of the test-suite
 setTestDirectory :: String -> Options -> IO Options
